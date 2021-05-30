7aa7939ed2c6a548ce7fe115911afb0a8dfd2f5f
pdhoward <patrick.howard@hotmail.com>
2021-05-30 08:11:39 -0500

    fixed exports

--- controllers/index.js
+++ controllers/index.js
@@ -1,24 +1,12 @@
 const {auth} =           	require('./auth')
 const {kafka} =             require('./kafka')
-const {findVenue,
-       fetchBrand,
-       findSubscriberAndUpdate,   
-       fetchStoreSample,
-       fetchTagSample,
-       fetchSubscribers,
-       fetchRandomSubscriber,
+const {fetchTagSample,       
        fetchRandomTag } =   require('./database')
 
 
 module.exports = {
 	auth,
-       kafka,
-	findVenue,
-       fetchBrand,
-       findSubscriberAndUpdate,   
-       fetchStoreSample,
-       fetchTagSample,
-       fetchSubscribers,
-       fetchRandomTag,
-       fetchRandomSubscriber
+       kafka,	
+       fetchTagSample,      
+       fetchRandomTag,      
   }
